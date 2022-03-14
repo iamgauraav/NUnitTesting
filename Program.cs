aa4cb8b3396f000d6cea15866573e6162765bf84
iamgauraav <iamgauraav@gmail.com>
2022-03-14 18:22:25 +0530

    Added Welcome Message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NUnitTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to NUnit Testing progam master branch");
+            Console.ReadLine();
+        }
+    }
+}

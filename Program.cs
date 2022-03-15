4a23c5f1f7ab4e649f7747720ffc8ad54dca16e4
iamgauraav <iamgauraav@gmail.com>
2022-03-15 16:13:11 +0530

    refactored code

--- Program.cs
+++ Program.cs
@@ -1,49 +1,33 @@
 ﻿using System;
 
 namespace NUnitTesting
-{
+{/// <summary>
+/// UC1- Vending machine
+/// </summary>
     class Program
-    {/// <summary>
-    /// UC1- Vending machine
-    /// </summary>
-
-        //Function to count
-        //Print currency notes
-        public static void FewestNotes(int amount)
-        {
-            int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
-            int[] count = new int[notes.Length];
-
-            //count notes using Greedy approach
-            for (int i = 0; i < notes.Length; i++)
-            {
-                if (amount >= notes[i])
-                {
-                    count[i] = amount / notes[i];
-                    amount = amount - (count[i] * notes[i]);
-
-                    if (amount < 0)
-                    {
-                        break;
-                    }
-                }
-            }
-            //print notes
-            for (int a = 0; a < notes.Length; a++)
-            {
-                if (count[a] != 0)
-                {
-                    Console.WriteLine(+count[a] + " no of notes of rupees " + notes[a]);
-                }
-            }
-        }
-        //Driver function
+    {
         static void Main(string[] args)
         {
-            int amount = 1000;
-            FewestNotes(amount);
-            Console.ReadLine();
+            int[] arrNotes = { 1000, 500, 100, 50, 20, 10, 5, 2, 1 };
 
+            Console.WriteLine("Enter the amount to get change \n(1000, 500, 100, 50, 5, 2, 1 available)");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            ////count notes using For loop
+            for (int i = 0; i < arrNotes.Length; i++)
+            {
+                int noOfNotes = 0;
+
+                if(amount >= arrNotes[i]);
+                {
+                    noOfNotes = amount / arrNotes[i];
+                    //Using % finding remaining change
+                    amount = amount % arrNotes[i];
+
+                    
+                    Console.WriteLine(+arrNotes[i] + " no of notes of rupees " + noOfNotes );
+                }
+            }
+            Console.ReadLine();
         }
         
     }

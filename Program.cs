181586ea841ec177fd1b04bb61b9fc735b550c29
iamgauraav <iamgauraav@gmail.com>
2022-03-16 23:21:08 +0530

    Added UC6 : convert decimal to binary

--- Program.cs
+++ Program.cs
@@ -2,34 +2,38 @@
 
 namespace NUnitTesting
 {/// <summary>
-/// UC-5 Calculating square root of  a number using newtons method
+/// UC-6 To Binary
 /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            //N is the numbeer for which you are calculating squareroot
-            Console.WriteLine("Enter a number");
-            double N = Convert.ToDouble(Console.ReadLine());
-            //set L is the tolerence level
-            double L = 0.00001;
-            double t = N;
-            double sqRoot;
-            int count = 0;
+            //local variable
+            int n, i;
 
-            while (true)
+            //array to store value 
+            int[] a = new int[10];
+
+            Console.Write("Enter the number to convert: ");
+            n = int.Parse(Console.ReadLine());
+            //for loop 
+            for (i = 0; n > 0; i++)
             {
-                count++;
-                sqRoot = 0.5 * (t + (N / t));
-
-                if (Math.Abs(sqRoot - t) < L)
-                {
-                    break;
-                }
-                t = sqRoot;
+                a[i] = n % 2;
+                n = n / 2;
             }
-            Console.WriteLine("Root Value is : " + Math.Round(sqRoot, 2));
+
+
+            Console.Write("Binary of the given number= ");
+
+            //for loop for binary
+            for (i = i - 1; i >= 0; i--)
+            {
+                Console.Write(a[i]);
+            }
             Console.ReadLine();
+
+            
         }
     }
 }

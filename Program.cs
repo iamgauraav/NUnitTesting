aba20e7cbdff4f0b9d5894b628d2095094ba6f57
iamgauraav <iamgauraav@gmail.com>
2022-03-16 17:02:25 +0530

    Added UC3 : Temperature Convertion

--- Program.cs
+++ Program.cs
@@ -1,34 +1,29 @@
 ﻿using System;
 
 namespace NUnitTesting
-{/// <summary>
-/// UC1- Vending machine
-/// </summary>
+{
     class Program
     {
         static void Main(string[] args)
         {
-            int[] arrNotes = { 1000, 500, 100, 50, 20, 10, 5, 2, 1 };
+            //for calling  method created obj of the class 
+            TemperatureConvertion temp = new TemperatureConvertion();
 
-            Console.WriteLine("Enter the amount to get change \n(1000, 500, 100, 50, 5, 2, 1 available)");
-            int amount = Convert.ToInt32(Console.ReadLine());
-            ////count notes using For loop
-            for (int i = 0; i < arrNotes.Length; i++)
-            {
-                int noOfNotes = 0;
+            //taking celsius value from user
+            Console.WriteLine("Please provide celsius temp:");
+            string val1 = Console.ReadLine();
+            //CelsiusTOFahrenheit using double so double Fahrenheit
+            double Fahrenheit = temp.CelsiusTOFahrenheit(val1);
+            //interpulated string to print values
+            Console.WriteLine($"The value of {val1} celsius is {Fahrenheit} Fahrenheit ");
 
-                if(amount >= arrNotes[i]);
-                {
-                    noOfNotes = amount / arrNotes[i];
-                    //Using % finding remaining change
-                    amount = amount % arrNotes[i];
-
-                    
-                    Console.WriteLine(+arrNotes[i] + " no of notes of rupees " + noOfNotes );
-                }
-            }
+            Console.WriteLine("Please provide Fahrenheit temp:");
+            string val2 = Console.ReadLine();
+            double celsius = temp.FahrenheitToCelsius(val2);
+            Console.WriteLine($"The value of {val2} Fahrenheit is {celsius} celsius");
             Console.ReadLine();
         }
         
     }
 }
+
---
+++ TemperatureConvertion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace NUnitTesting
+{
+    public class TemperatureConvertion
+    {
+        //Method to calculate CelsiusTOFahrenheit
+        public double CelsiusTOFahrenheit(string celsiusTemp)
+        {
+            double celsius = double.Parse(celsiusTemp);
+            double Fahrenheit = (celsius * 9 / 5) + 32;
+            return Fahrenheit;
+
+        }
+        // Method to calculate FahrenheitToCelsius
+        public double FahrenheitToCelsius(string fahrenheitTemp)
+        {
+            double fahrenheit = double.Parse(fahrenheitTemp);
+            double celsius = (fahrenheit - 32) * 5 / 9;
+            return celsius;
+        }
+    }
+}

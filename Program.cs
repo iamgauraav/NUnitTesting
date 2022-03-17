3c64c808c9ba02623636132898cd29a184ada161
iamgauraav <iamgauraav@gmail.com>
2022-03-17 18:16:07 +0530

    Added UC7 : Swap nibble

--- Program.cs
+++ Program.cs
@@ -1,58 +1,42 @@
 ﻿using System;
 
 namespace NUnitTesting
-{/// <summary>
-/// UC- 2 Day Of Week
-/// </summary>
+{
     class Program
     {
-        public static void UserInput()
-        {
-            Console.WriteLine("Enter Date");
-            int d =Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Month");
-            int m = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Year");
-            int y = Convert.ToInt32(Console.ReadLine());
-        }
-        public static void CalculateDay(int d ,int m , int y)
-        {
-            int y0 = y - (14 - m) / 12;
-            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
-            int m0 = m + 12 * ((14 - m) / 12) -2;
-            int d0 = (d + x + 31 * m0 / 12) % 7;
 
-            //switch case
-            switch (d0)
+        public static void SwapNibbles()
+        {
+            Console.WriteLine("Enter a value");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int result = ((x & 0x0F) << 4 | (x & 0xF0) >> 4);
+            Console.WriteLine("After swapping a bits " + result);
+            //turnary operator use-> method(variable) ? is like if condition,: is like else .
+            Console.WriteLine(isPowerOfTwo(result) ? "Number is power of 2" : "Number is not a power of 2");
+        }
+
+        // Method to check if result is power of 2
+        public static bool isPowerOfTwo(int result)
+        {
+            if (result == 0)
             {
-                case 0:
-                    Console.WriteLine("Sunday");
-                    break;
-                case 1:
-                    Console.WriteLine("Monday");
-                    break;
-                case 2:
-                    Console.WriteLine("Tuesday");
-                    break;
-                case 3:
-                    Console.WriteLine("Wednessday");
-                    break;
-                case 4:
-                    Console.WriteLine("Thrusday");
-                    break;
-                case 5:
-                    Console.WriteLine("Friday");
-                    break;
-                case 6:
-                    Console.WriteLine("Saturday");
-                    break;
+                return false;
             }
+            while (result != 1)
+            {
+                if (result % 2 != 0)
+                {
+                    return false;
+                }
+                result = result / 2;
+            }
+            return true;
         }
         static void Main(string[] args)
         {
-            Program cal = new Program();
-            UserInput();
-            Program.CalculateDay(12,04,2015);
+            Program swap = new Program();
+            SwapNibbles();
+            isPowerOfTwo(12);
             Console.ReadLine();
         }
     }

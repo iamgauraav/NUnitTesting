55dc83459253a932f9911184b614adef1d55b8dc
iamgauraav <iamgauraav@gmail.com>
2022-03-17 17:01:17 +0530

    Added UC2 : To calculte day by entering date

--- Program.cs
+++ Program.cs
@@ -2,33 +2,59 @@
 
 namespace NUnitTesting
 {/// <summary>
-/// UC1- Vending machine
+/// UC- 2 Day Of Week
 /// </summary>
     class Program
     {
+        public static void UserInput()
+        {
+            Console.WriteLine("Enter Date");
+            int d =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Month");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Year");
+            int y = Convert.ToInt32(Console.ReadLine());
+        }
+        public static void CalculateDay(int d ,int m , int y)
+        {
+            int y0 = y - (14 - m) / 12;
+            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            int m0 = m + 12 * ((14 - m) / 12) -2;
+            int d0 = (d + x + 31 * m0 / 12) % 7;
+
+            //switch case
+            switch (d0)
+            {
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednessday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thrusday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
-            int[] arrNotes = { 1000, 500, 100, 50, 20, 10, 5, 2, 1 };
-
-            Console.WriteLine("Enter the amount to get change \n(1000, 500, 100, 50, 5, 2, 1 available)");
-            int amount = Convert.ToInt32(Console.ReadLine());
-            ////count notes using For loop
-            for (int i = 0; i < arrNotes.Length; i++)
-            {
-                int noOfNotes = 0;
-
-                if(amount >= arrNotes[i]);
-                {
-                    noOfNotes = amount / arrNotes[i];
-                    //Using % finding remaining change
-                    amount = amount % arrNotes[i];
-
-                    
-                    Console.WriteLine(+arrNotes[i] + " no of notes of rupees " + noOfNotes );
-                }
-            }
+            Program cal = new Program();
+            UserInput();
+            Program.CalculateDay(12,04,2015);
             Console.ReadLine();
         }
-        
     }
 }
+

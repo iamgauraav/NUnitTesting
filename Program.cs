401cc4e7193d799063b5c181bea85193bc7112e1
iamgauraav <iamgauraav@gmail.com>
2022-03-17 17:00:09 +0530

    Added UC2 : To calculte day by entering date

--- Program.cs
+++ Program.cs
@@ -2,38 +2,58 @@
 
 namespace NUnitTesting
 {/// <summary>
-/// UC-6 To Binary
+/// UC- 2 Day Of Week
 /// </summary>
     class Program
     {
+        public static void UserInput()
+        {
+            Console.WriteLine("Enter Date");
+            int d =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Month");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Year");
+            int y = Convert.ToInt32(Console.ReadLine());
+        }
+        public static void CalculateDay(int d ,int m , int y)
+        {
+            int y0 = y - (14 - m) / 12;
+            int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+            int m0 = m + 12 * ((14 - m) / 12) -2;
+            int d0 = (d + x + 31 * m0 / 12) % 7;
+
+            //switch case
+            switch (d0)
+            {
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednessday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thrusday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
-            //local variable
-            int n, i;
-
-            //array to store value 
-            int[] a = new int[10];
-
-            Console.Write("Enter the number to convert: ");
-            n = int.Parse(Console.ReadLine());
-            //for loop 
-            for (i = 0; n > 0; i++)
-            {
-                a[i] = n % 2;
-                n = n / 2;
-            }
-
-
-            Console.Write("Binary of the given number= ");
-
-            //for loop for binary
-            for (i = i - 1; i >= 0; i--)
-            {
-                Console.Write(a[i]);
-            }
+            Program cal = new Program();
+            UserInput();
+            Program.CalculateDay(12,04,2015);
             Console.ReadLine();
-
-            
         }
     }
 }
